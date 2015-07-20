7faec84e4840a517f4da3b7e9a35fcc0820b4e16
Adrian Kuhn <akuhn@iam.unibe.ch>
2015-07-20 00:57:41 -0700

    Adding rspec examples.

--- spec/methodsolver_spec.rb
+++ spec/methodsolver_spec.rb
@@ -6,11 +6,62 @@ describe Methodsolver do
     expect(Methodsolver::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
+  it 'should pass README.md example' do
     object, found = Methodsolver.call { 'lettuce'.foo == 7 }
 
     expect(found).to include :size
     expect(found).to include :length
+  end
+
+  it 'should fail when no block given' do
+    expect {
+      Methodsolver.call
+    }.to raise_error ArgumentError
+  end
+
+  it 'should fail when no placeholder given' do
+    expect {
+      Methodsolver.call { 3 + 4 == 7 }
+    }.to raise_error ArgumentError
+  end
+
+  let(:words) do
+    %w(the quick brown fox jumps over the lazy dog)
+  end
+
+  it 'should find methods without arguments' do
+    object, found = Methodsolver.call {
+      words.dup.foo == 'the'
+    }
+    expect(found).to include :first
+  end
+
+  it 'should find methods with an argument' do
+    object, found = Methodsolver.call {
+      words.dup.foo(/...../) == %w(quick brown jumps)
+    }
+    expect(found).to include :grep
+  end
+
+  it 'should find methods with many arguments' do
+    object, found = Methodsolver.call {
+      words.dup.foo(1, 2, 4) == %w(quick brown jumps)
+    }
+    expect(found).to include :values_at
+  end
+
+  it 'should find methods with block argument' do
+    object, found = Methodsolver.call {
+      words.dup.foo(&:size) == [3, 5, 5, 3, 5, 4, 3, 4, 3]
+    }
+    expect(found).to include :collect
+  end
+
+  it 'should find methods that raise errors' do
+    object, found = Methodsolver.call {
+      words.dup.freeze.foo rescue true
+    }
+    expect(found).to include :clear
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'methodsolver'
+
+RSpec.configure do |config|
+  config.order = 'random'
+end
